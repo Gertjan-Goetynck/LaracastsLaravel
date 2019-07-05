ee3b4237c8a2941ffc72249fd92017ccecfd0672
Gertjan <gertjan@goetynck.com>
2019-07-05 10:31:39 +0200

    Lesson 13

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('layout')
+
+@section('content')
+<h1 class="title">Edit project</h1>
+<form action="/projects/{{$project->id}}" method="POST" style="margin-bottom:1rem;">
+    <!-- {{ csrf_field() }}
+    {{ method_field('PUT') }} -->
+    @medthod('DELETE')
+    @csrf
+    <div class="field">
+        <label class="label" for="title">Title</label>
+        <div class="control">
+            <input type="text" class="input" name="title" placeholder="Title" value="{{ $project->title }}">
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label" for="description">Description</label>
+        <div class="control">
+        <textarea name="description" class="textarea" id="description">{{$project->description}}</textarea>
+        </div>
+    </div>
+    <div class="field">
+        <div class="control">
+            <button type="submit" class="button is-link">Update project</button>
+        </div>
+    </div>
+</form>
+<form action="/projects/{{$project->id}}" method="POST">
+    {{ csrf_field() }}
+    {{ method_field('DELETE') }}
+    <div class="field">
+        <div class="control">
+            <button type="submit" class="button">Delete project</button>
+        </div>
+    </div>
+</form>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+/*
+    GET /projects (index) GET ALL
+    POST /projects (store) SAVE NEW PROJECT
+    GET /projects/create (get) GO TO CREATE PROJECT FORM
+    PUT /projects/{id} (update)
+    GET /projects/{id}/edit (edit)
+    DELETE /projects/{id} (destroy)
+*/
+
+//Route::resource('projects','ProjectsController');
+
+Route::get('/projects','ProjectsController@index');
+Route::get('/projects/{id}','ProjectsController@show');
+Route::get('/projects/create','ProjectsController@create');
+Route::post('/projects','ProjectsController@store');
+Route::get('/projects/{id}/edit','ProjectsController@edit');
+Route::put('/projects/{id}','ProjectsController@update');
+Route::delete('/projects/{id}','ProjectsController@destroy');
